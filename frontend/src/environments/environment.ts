b895b794b7b12faa29cc84fbefbf0ca4ee328439
Dv-2006-ss <dhairya@microtrust.dev>
2026-04-19 23:53:07 +0530

    update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts  (development)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: '{environment.apiUrl}/analyze'
 };
